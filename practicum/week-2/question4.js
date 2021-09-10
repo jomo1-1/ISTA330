104140a48337dab572bfb2ef9fad80dc909234b2
jomo1-1 <89543295+jomo1-1@users.noreply.github.com>
2021-09-09 23:39:51 -0700

    Update 2 question4.js

--- practicum/week-2/question4.js
+++ practicum/week-2/question4.js
@@ -1,5 +1,5 @@
 /*
-You are given strings A and B.  
+You are given strings A and B.
 How many of the letters in B are also present in A?
 
 The letters in A are guaranteed distinct, and all characters in A and B are letters.
@@ -10,6 +10,29 @@ The letters in A are guaranteed distinct, and all characters in A and B are lett
  output: 3
 */
 
-var howManyCommon = function(A, B) {
+/*
+  let letters = 0;
+  function repeat (A, f) {
+  for (let i = 0; i < A.length; i++) {
+    f(i);
+      if (A[i] == f[i]) {
+        letters++;
+          }
+      }
+  }
 
+  return letters;
+
+*/
+
+var howManyCommon = function(A, B) {
+  let letters = 0;
+  for (let i = 0, j = 0; i < A.length && j < B.length; i++, j++) {
+      if (A[i] == B[j]) {
+        letters++;
+      } else {
+        return;
+      }
+    }
+  return letters;
 };
