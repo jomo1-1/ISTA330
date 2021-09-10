b3f717a1f0a699216e8e1ac6d1b76d7b2e83d529
jomo1-1 <89543295+jomo1-1@users.noreply.github.com>
2021-09-09 23:44:00 -0700

    Update 4 question4.js

--- practicum/week-2/question4.js
+++ practicum/week-2/question4.js
@@ -27,7 +27,7 @@ The letters in A are guaranteed distinct, and all characters in A and B are lett
 
 var howManyCommon = function(A, B) {
   let letters = 0;
-  for (let i = 0, j = 0; i < A.length && j < B.length; i++, j++) {
+  for (let i = 0, j = 0; i < A.length, j < B.length; i++, j++) {
       if (A[i] == B[j]) {
         letters++;
       }
