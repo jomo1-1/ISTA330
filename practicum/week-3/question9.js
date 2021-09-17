e112ab6d2793b7a8176ccd5b8b0ffc0f9a4e987d
jomo1-1 <89543295+jomo1-1@users.noreply.github.com>
2021-09-17 00:33:36 -0700

    update q9

--- practicum/week-3/question9.js
+++ practicum/week-3/question9.js
@@ -1,7 +1,7 @@
 /*
-Given an integer n, 
-put each number from 1 to n 
-into groups based on the sum of its digits. 
+Given an integer n,
+put each number from 1 to n
+into groups based on the sum of its digits.
 
 Return how many groups have the largest size.
 
@@ -13,5 +13,27 @@ so there are two groups with the largest size.
 */
 
 var largestGroupsCount = function(n) {
+  let map = new Map();
 
+  for (let i = 1; i <= n ; i++) {
+    let num = i
+    let sum = 0;
+    while (num > 0) {
+      sum += num % 10;
+      num = Math.floor(num / 10);
+    }
+      map.set(sum, (map.get(sum) || 0) + 1);
+  }
+
+  let result = new Map();
+  for (let [kk, val] of map.entries()) {
+    result.set(val, (result.get(val) || 0 ) + 1)
+  }
+
+  let finder = 0;
+  for (let [km, vm] of result.entries()) {
+    finder = Math.max(finder, km);
+  }
+
+  return result.get(finder);
 };
