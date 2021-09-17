f0a10aeda39bbea32c85ee920335812114e9e1f0
jomo1-1 <89543295+jomo1-1@users.noreply.github.com>
2021-09-16 23:59:59 -0700

    update q8 (v3)

--- practicum/week-3/question8.js
+++ practicum/week-3/question8.js
@@ -1,9 +1,9 @@
 /*
-Given an array of integers, input, 
-a d-integer is an integer which has 
+Given an array of integers, input,
+a d-integer is an integer which has
 a frequency in the array equal to its value.
 
-Return a d-integer. If there are multiple 
+Return a d-integer. If there are multiple
 d-integers return the largest of them.
 If there is no d-integer return -1.
 
@@ -13,5 +13,23 @@ output: 3
 */
 
 var d_integer = function(input) {
+  let map = new Map();
+  for (let i = 0; i < input.length; i++) {
+    map.set(input[i], map.get(input[i]) + 1 || 1);
+  }
 
+  const sortm = new Map([...map.entries()].sort((a, b) => b[1] - a[1]));
+
+  /*
+  const sortm = new Map([...map.entries()].sort(function(a, b) {
+    b[1] - a [1]
+  }));
+  */
+
+  for (let [key, value] of sortm) {
+    if (key === value){
+      return key;
+      }
+  }
+  return -1;
 };
