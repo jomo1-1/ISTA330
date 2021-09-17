1af8a31faced811e653ba420e9ed2d0de89976e5
jomo1-1 <89543295+jomo1-1@users.noreply.github.com>
2021-09-16 20:35:08 -0700

    Update question2.js

--- practicum/week-3/question2.js
+++ practicum/week-3/question2.js
@@ -10,16 +10,12 @@ Example: input: [-12,3,-1,5,-2,1,-7]
           */
 
  var largestSubarray = function(input) {
-   const sum1 = Math.min(input);
-   const sum = 0;
-   for (var i = 0; i < input.length; i++) {
-     if (sum < 0) {
-       continue
-     } else {
-        sum += i;
-        sum1 = Math.max(sum, sum1);
-     }
-     return sum1;
-   }
+   let sum = 0;
+   let largestSum = 0;
 
+   for (let i = 0; i < input.length; i++) {
+     sum = Math.max(0, (sum + i));
+     largestSum = Math.max(largestSum, sum);
+   }
+   return largestSum;
  };
