3b8eeb6fd5e0a9c6d3f26c9f17bb54449e8a2f4e
jomo1-1 <89543295+jomo1-1@users.noreply.github.com>
2021-09-16 23:12:24 -0700

    update q7 (v2)

--- practicum/week-3/question7.js
+++ practicum/week-3/question7.js
@@ -1,16 +1,24 @@
 /*
 Given a matrix M, return the transpose of M.
 
-The transpose of a matrix is a 
-new matrix whose rows are the columns 
+The transpose of a matrix is a
+new matrix whose rows are the columns
 of the original.
 
-Example: 
+Example:
 input: [[1,2,3],[4,5,6]]
 output: [[1,4], [2,5], [3,6]]
 
 */
 
 var transpose = function(M) {
-
+  let arr = [];
+  for (let i = 0; i < M[0].length; i++) {
+    let col = [];
+    for (let x = 0; x < M.length; x++) {
+      col.push(M[x][i]);
+     }
+    arr.push(col);
+    }
+  return arr;
 };
