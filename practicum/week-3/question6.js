68d9d230bf458585b8770c138223ea85bb23dd44
jomo1-1 <89543295+jomo1-1@users.noreply.github.com>
2021-09-16 22:48:00 -0700

    update q6

--- practicum/week-3/question6.js
+++ practicum/week-3/question6.js
@@ -1,16 +1,19 @@
 /*
-Given an array of size n, 
-find the m-element. 
-The m-element is 
+Given an array of size n,
+find the m-element.
+The m-element is
 the element that appears more than ⌊ n/2 ⌋ times.
 
 If the m-element does not exist return -1.
 
-Example: 
+Example:
 input: [1, 2, 2, 3, 2, 7, 2]
-output: 2 
+output: 2
 */
 
 var m_element = function(input) {
-
+  input = input.sort(function(n1, n2) {
+    n1 - n2
+  });
+  return(input[Math.floor(input.length / 2)]);
 };
